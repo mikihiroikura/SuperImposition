24bfd15e3a483bf4206e5ab2526dc98b6c750871
mikihiroikura <miki.ut.sikepuri@gmail.com>
2021-05-06 19:40:20 +0900

    差分位置姿勢の計算，そもそも前提が間違えていた

--- EvalPoseAccuracy/EvalMarkerPose.m
+++ EvalPoseAccuracy/EvalMarkerPose.m
@@ -95,71 +95,30 @@ for i = 1:size(RT_cb2mk,3)
     end
 end
 
-%hsc2mkの初期位置姿勢からの変動を計算
-RT_mkinit2mki_at_mki = zeros(4,4,size(RT_hsc2mk,3));
-for k = 1:size(RT_hsc2mk,3)
-    if RT_hsc2mk(4,1,k)~=0 
-        RT_mkinit2mki_at_mki(:,:,k) = RT_hsc2mk(:,:,1) \ (RT_hsc2mk(:,:,k));
-    end
+%CB2MKの変動を計算
+RTdiff_cb2mk = zeros(size(RT_cb2mk));
+Rvecdiff_cb2mk = zeros(size(RT_cb2mk,3),3);
+for i=1:size(RT_cb2mk,3)
+    RTdiff_cb2mk(:,:,i) = RT_cb2mk(:,:,1) / RT_cb2mk(:,:,i);
+    Rvecdiff_cb2mk(i,:) = rotationMatrixToVector(RTdiff_cb2mk(1:3,1:3,i)) * 180 /pi;
 end
-
-%CBの位置変動@CBiをMKi座標系に変換
-T_cbinit2cbi_at_mki = zeros(size(RT_cb2mk,3),3);
-hsctimeid = 1;
-for i = 1:size(RT_cb2mk,3)
-    while M_hsctime_used(hsctimeid)<ledtime(i) && hsctimeid < size(M_hsctime_used,1)
-        hsctimeid = hsctimeid + 1;
-    end
-    if RT_cbinit2cbi(4,1,hsctimeid)~=0 && RT_cb2mk(4,1,i)~=0
-        T_cbinit2cbi_at_mki(i,:) = RT_cbinit2cbi(4,1:3,hsctimeid) * RT_cb2mk(1:3,1:3,i);
-    end
-end
-%CBの姿勢変動@CbiをMki座標系に変換
-Rvec_cbinit2cbi_at_mki = zeros(size(RT_cb2mk,3),3);
-R_cbinit2cbi_at_mki = zeros(3, 3, size(RT_cb2mk,3));
-hsctimeid = 1;
-for i = 1:size(Rvec_cbinit2cbi_at_mki,1)
-    while M_hsctime_used(hsctimeid)<ledtime(i) && hsctimeid < size(M_hsctime_used,1)
-        hsctimeid = hsctimeid + 1;
-    end
-    if RT_cb2mk(4,1,i)~=0 && RT_cbinit2cbi(4,1,hsctimeid)~=0
-        Rvec_cbinit2cbi_at_cbi = rotationMatrixToVector(RT_cbinit2cbi(1:3,1:3,hsctimeid));
-        Rvec_cbinit2cbi_at_mki(i,:) = Rvec_cbinit2cbi_at_cbi * RT_cb2mk(1:3,1:3,i);
-        R_cbinit2cbi_at_mki(:,:,i) = rotationVectorToMatrix(Rvec_cbinit2cbi_at_mki(i,:));
-    end
-end
-
-%CBの姿勢変動@MkiとMkの姿勢変動@Mkiの差分計算
-Rdiff_cb2mk_at_mki = zeros(3,3,size(RT_mkinit2mki_at_mki,3));
-Rvec_diff_cb2mk_at_mki = zeros(size(RT_mkinit2mki_at_mki,3),3);
-for i = 1:size(Rdiff_cb2mk_at_mki,3)
-    if R_cbinit2cbi_at_mki(1,1,i)~=0 && RT_mkinit2mki_at_mki(4,1,i)~=0
-        Rdiff_cb2mk_at_mki(:,:,i) = R_cbinit2cbi_at_mki(:,:,i) / RT_mkinit2mki_at_mki(1:3,1:3,i);
-        Rvec_diff_cb2mk_at_mki(i,:) = rotationMatrixToVector(Rdiff_cb2mk_at_mki(:,:,i)) * 180 /pi;
-    end
-end
-
-%位置ずれ差分計算
-Tdiff_cb2mk_at_mki = zeros(size(RT_mkinit2mki_at_mki,3),3);
-for i = 1:size(Tdiff_cb2mk_at_mki,1)
-    Tdiff_cb2mk_at_mki(i,:) = T_cbinit2cbi_at_mki(i,:)-squeeze(RT_mkinit2mki_at_mki(4,1:3,i));
-end
-
 
 %位置変動出力
-for k = 1:3
-    figure
-    plot(ledtime,T_cbinit2cbi_at_mki(:,k));
-    hold on
-    plot(ledtime,squeeze(RT_mkinit2mki_at_mki(4,k,:)));
-end
-%姿勢ずれ出力
 figure
 for k = 1:3
-    plot(ledtime,Rvec_diff_cb2mk_at_mki(:,k))
+    plot(ledtime,squeeze(RTdiff_cb2mk(4,k,:)));
+    hold on
+end
+
+%姿勢変動出力
+figure
+for k = 1:3
+    plot(ledtime,(Rvecdiff_cb2mk(:,k)));
     hold on
 end
 
 %ずれの平均分散を計算
-% mean(Tdiff_cb2mk_at_mki);
-% std(Tdiff_cb2mk_at_mki);
+% mean(squeeze(RTdiff_cb2mk(4,1:3,:)).');
+% std(squeeze(RTdiff_cb2mk(4,1:3,:)).');
+% mean(Rvecdiff_cb2mk(:,:));
+% std(Rvecdiff_cb2mk(:,:));
